1bbb1199c5e90a32e07b0205d626c11bc6cd4f5d
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2023-10-07 10:04:18 +0200

    responsive design

--- src/pages/Bio.js
+++ src/pages/Bio.js
@@ -6,7 +6,11 @@ function Bio() {
       <div id='about'>
         <article className='about-text'>
           <h2>Mon Histoire</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p>Je suis un jeune développeur front-end, à la recherche d’emploi suite à l’achèvement de mes études.
+              Pendant les dernières deux années j'ai été formé comme « développeur d’application React » avec Openclassrooms (RNCP35976 - Développeur concepteur logiciel). Pendant cette formation, j'ai été formé aux langages HTML5, CSS3 et JavaScript, et j'ai acquis des compétences essentielles dans le développement front-end. De plus, j'ai approfondi mes connaissances en utilisant des frameworks tels que Redux pour gérer l'état de l'application de manière efficace, ainsi que Sass pour améliorer la maintenabilité et la lisibilité du code CSS.
+              En complément, j'ai appris à interagir avec des API REST pour récupérer et envoyer des données de manière asynchrone, ce qui m'a permis de développer des applications réactives et dynamiques.
+              Je suis constant, autonome et rigoureux. Mon expérience professionnelle passée m'a permis de développer des compétences pour gérer avec succès des situations complexes et stressantes, tout en respectant des délais serrés.
+          </p>
         </article>
         <aside>
           
