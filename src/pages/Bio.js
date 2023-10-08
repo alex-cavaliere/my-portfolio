fcbde7c075b84abd0ea282bc252832aa9985f56e
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2023-10-08 21:59:26 +0200

    fixing layout shifting

--- src/pages/Bio.js
+++ src/pages/Bio.js
@@ -10,6 +10,7 @@ function Bio() {
               Pendant les dernières deux années j'ai été formé comme « développeur d’application React » avec Openclassrooms (RNCP35976 - Développeur concepteur logiciel). Pendant cette formation, j'ai été formé aux langages HTML5, CSS3 et JavaScript, et j'ai acquis des compétences essentielles dans le développement front-end. De plus, j'ai approfondi mes connaissances en utilisant des frameworks tels que Redux pour gérer l'état de l'application de manière efficace, ainsi que Sass pour améliorer la maintenabilité et la lisibilité du code CSS.
               En complément, j'ai appris à interagir avec des API REST pour récupérer et envoyer des données de manière asynchrone, ce qui m'a permis de développer des applications réactives et dynamiques.
               Je suis constant, autonome et rigoureux. Mon expérience professionnelle passée m'a permis de développer des compétences pour gérer avec succès des situations complexes et stressantes, tout en respectant des délais serrés.
+              En somme, mon parcours de formation et mon expérience professionnelle passée m'ont préparé à relever les défis du développement front-end avec détermination. Je suis impatient de mettre en pratique mes compétences et ma passion pour la technologie au sein d'une équipe dynamique et innovante.
           </p>
         </article>
         <aside>
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -23,7 +23,7 @@ function HomePage() {
     <div id="home">
       <section id="experiences">
         <article>
-          <h2>Mes Experience</h2>
+          <h1>Mes Experience</h1>
           <div className='card'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
         </article>
       </section>
