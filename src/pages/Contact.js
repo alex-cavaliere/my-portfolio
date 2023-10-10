bc3cacb000cafafee26981ea9acf3f89f5bb6383
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2023-10-10 18:40:31 +0200

    v1.0 finalisation

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,20 @@
 import '../css/style.css';
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 function Contact() {
   return (
     <div>
-      <div id='contact'>
+      <div id='contacts'>
         <h1>Contacts</h1>
+        <div className='contacts-container'>
+          <ul className='contacts-items'>
+            <li className='item'><span class="fi fi-fr"></span>tel:+33 0767688211</li>
+            <li className='item'><span class="fi fi-it"></span>tel:+39 3477728839</li>
+          </ul>
+          <p className='item-email'>
+            <a href='mailto:alexcavaliere517@gmail.com'>alexcavaliere517@gmail.com</a>
+          </p>
+        </div>
       </div>
     </div>
   )
