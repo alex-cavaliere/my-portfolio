01bcf252e716be2e6a9846cd6edcae17d6a1781c
Alessandro Cavaliere <alexcavaliere517@gmail.com>
2025-04-10 09:27:10 +0200

    Bug fix display mobile menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,12 +11,21 @@ function Header() {
       y.style.backgroundColor = '#051E44'
       y.style.color = 'white'
       y.style.transition = 'background-color 250ms ease-in-out, color 250ms ease-in-out'
+      
     }else{
       x.className = 'navbar'
       y.style.backgroundColor = 'white'
       y.style.color = 'black'
       y.style.transition = 'background-color 250ms ease-in-out, color 250ms ease-in-out'
     }
+  }
+  const closeNavbar = () => {
+    const x = document.querySelector('nav')
+    const y = document.getElementById('icon')
+    x.className = 'navbar'
+    y.style.backgroundColor = 'white'
+    y.style.color = 'black'
+    y.style.transition = 'background-color 250ms ease-in-out, color 250ms ease-in-out'
   }
   return (
     <header>
@@ -25,9 +34,9 @@ function Header() {
       </div>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <NavLink to='/' onClick={editNavbar}>Homepage</NavLink>
-          <NavLink to='/info' onClick={editNavbar}>info </NavLink>
-          <NavLink to='/contact' onClick={editNavbar}>contact</NavLink>
+          <NavLink to='/' onClick={closeNavbar}>Homepage</NavLink>
+          <NavLink to='/info' onClick={closeNavbar}>info </NavLink>
+          <NavLink to='/contact' onClick={closeNavbar}>contact</NavLink>
         </div>
         <span id='icon' onClick={editNavbar}>
           <i className='line-1'></i>
